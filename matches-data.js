38254d01092928b65a2d3d496e05b2c93f29b72b
thotamahesh0101 <thotamahesh0101@gmail.com>
2026-09-13 14:46:03 +0530

    Create matches-data.js

---
+++ matches-data.js
@@ -0,0 +1,28 @@
+const matches = [
+  {
+    tournament: "International",
+    team1: "India",
+    team2: "Australia",
+    time: "7:30 PM",
+    venue: "Hyderabad",
+    status: "UPCOMING"
+  },
+
+  {
+    tournament: "International",
+    team1: "England",
+    team2: "South Africa",
+    time: "10:00 PM",
+    venue: "London",
+    status: "UPCOMING"
+  },
+
+  {
+    tournament: "T20 League",
+    team1: "Mumbai",
+    team2: "Chennai",
+    time: "3:30 PM",
+    venue: "Mumbai",
+    status: "UPCOMING"
+  }
+];
